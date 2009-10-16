c3fd321d953f6e63faeb486162f63f3d76a92224
Daniel de la Mata <danimata@aspgems.com>
2009-10-16 19:30:30 +0200

    feeds and entries... needs polishing

---
+++ app/models/feed.rb
@@ -0,0 +1,42 @@
+class Feed < ActiveRecord::Base
+  belongs_to :user
+  has_many :entries, :dependent => :destroy
+  #after_create :fetch!
+  
+  validates_presence_of :feed_url
+  validates_uniqueness_of :feed_url
+  validates_format_of :feed_url, :with => REXP_URL, :allow_nil => false, :allow_blank => false
+  
+  #
+  # Fetch feed, update attibutes and create entries
+  #
+  def fetch!
+    parsed_feed = FeedNormalizer::FeedNormalizer.parse open(self.feed_url)
+    
+    self.update_attributes( :title => parsed_feed.title,
+                            :url => parsed_feed.url
+                            #:etag => parsed_feed.etag
+                            #:last_modified => parsed_feed.last_modified
+                          )
+                            
+    parsed_feed.entries.each do |entry|
+      self.entries.create(:url => entry.url,
+                          :title => entry.title,
+                          :author => entry.author,
+                          #:summary => entry.summary,
+                          :content => entry.content
+                          #:published => entry.published
+                          #:categories => entry.categories
+                        ) if !Entry.find_by_url(entry.url)
+    end
+  end
+  
+  #
+  # Fetch all feeds
+  #
+  def self.fetch_all!
+    Feed.find(:all).each do |f|
+      f.fetch! rescue nil
+    end
+  end
+end
---
+++ db/migrate/20091016160900_create_feeds.rb
@@ -0,0 +1,17 @@
+class CreateFeeds < ActiveRecord::Migration
+  def self.up
+    create_table :feeds do |t|
+      t.string :title
+      t.string :url
+      t.string :feed_url
+      t.string :etag
+      t.datetime :last_modified
+      t.timestamps
+    end
+  end
+ 
+  def self.down
+    drop_table :feeds
+  end
+end
+ 
